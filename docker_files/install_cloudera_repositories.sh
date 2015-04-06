5c30e5a9e102cc9f5f9bdf8a3b12476c09a17368
caioquirino <caioquirino@gmail.com>
2015-04-06 00:18:38 -0300

    Fixed Cloudera repository installation

--- docker_files/install_cloudera_repositories.sh
+++ docker_files/install_cloudera_repositories.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+echo 'Installing Cloudera repositories...'
+
 curl -s http://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh/archive.key | apt-key add -
 
 echo 'deb [arch=amd64] http://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh trusty-cdh5 contrib' > /etc/apt/sources.list.d/cloudera.list
 echo 'deb-src http://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh trusty-cdh5 contrib' >> /etc/apt/sources.list.d/cloudera.list
+
+echo 'CloudeC repositories Installed.'
